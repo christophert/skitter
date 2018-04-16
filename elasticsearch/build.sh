ef2a19d56f33f3a7634ad9d84fb7c01138ddfa4e
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-16 09:22:59 -0400

    Current progress on act7

---
+++ elasticsearch/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+KEYSTORE_PASSWORD=$(cat private-pass)
+
+# generate certificates
+#/gocode/bin/cfssl gencert -remote=http://ca:8888 \
+#    -hostname=authentication csr.json
+
+# get CA Certificate
+curl -XPOST -H "Content-Type: application/json" -d '{"label": "primary"}' \
+    http://ca:8888/api/v1/cfssl/info > ca.json
+
+curl -X POST -H "Content-Type: application/json" -d @csr.json \
+    http://ca:8888/api/v1/cfssl/newcert > full_cert.json
+
+# my jank method of extracting json content b/c it takes way too long to install
+# modules (~5 minutes is to long) -- this is native to unix
+extract_json_data () {
+    cat $1 | \
+        grep -Po "\"$2\""':.*?[^\\]\",' | \
+        sed 's/"'"$2"'":"//g' | \
+        sed ':a;N;$!ba;s/\n/ /g' | \
+        sed 's/",.*//' | \
+        sed 's/\\n/\n/g' > $3
+}
+echo $KEYSTORE_PASSWORD
+# make the private key and certificate request
+extract_json_data ca.json certificate ca.crt
+extract_json_data full_cert.json certificate certificate.crt
+extract_json_data full_cert.json private_key certificate.key
+
+cat ca.crt
+cat certificate.crt
+
+cat ca.crt >> certificate.crt
+
+# export as pkcs12
+openssl pkcs12 -export -name elasticsearchssl -in certificate.crt \
+    -inkey certificate.key -out /ssl/keystore.p12 \
+    -password pass:$KEYSTORE_PASSWORD
+
+# make a keystore for our application and store it in the correct folder
+keytool -importkeystore -destkeystore /ssl/keystore.jks \
+    -srckeystore /ssl/keystore.p12 -srcstoretype pkcs12 \
+    -alias elasticsearchssl -keypass $KEYSTORE_PASSWORD \
+    -storepass $KEYSTORE_PASSWORD -srckeypass $KEYSTORE_PASSWORD \
+    -destkeypass $KEYSTORE_PASSWORD < private-pass
+
+# clean unessessary files
+rm ca.json full_cert.json ca.crt certificate.crt certificate.key 
+
+login elasticsearch
+# run elasticsearch
+elasticsearch
+
--- elasticsearch/docker-run.sh
+++
@@ -1,3 +0,0 @@
-docker build -t my-es-image .
-#docker volume create --name esdata
-docker run --rm -ti -p 9200:9200 -v esdata:/usr/share/elasticsearch/data my-es-image
