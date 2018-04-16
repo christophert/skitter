dd073702536d808a8e7778b1f2fec7c8d813c368
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-16 17:41:10 -0400

    Fix routing issues, need to fix login.js

--- web/skitterapp/src/components/Login.js
+++ web/skitterapp/src/components/Login.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react';
 
-const LoginComponent = () => (
+class Login extends Component {
+  render() {
+    return (
+            <div>
     <form class="form-signin">
         <h1 class="h3 mb-3 font-weight-bold">login</h1>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="text" id="username" class="form-control" placeholder="Username" required autofocus>
+        <input type="text" id="username" class="form-control" placeholder="Username" required autofocus> </input>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="password" class="form-control" placeholder="Password" required>
+        <input type="password" id="password" class="form-control" placeholder="Password" required> </input>
         <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> Remember me </input>
         </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block bg-purple" type="submit">Log in</button>
         <hr />
         <p>New to skittr? <a href="/register">Sign up now</a></p>
     </form>
-)
+    </div>
+    );
+  }
+}
 
-const LoginRouter = () => (
-    <Route exact path='/login' component={LoginComponent}/>
-)
-
-export default LoginRouter
+export default Login;
--- web/skitterapp/src/index.js
+++ web/skitterapp/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './components/App';
+import Header from './components/Header';
+import Login from './components/Login';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>), document.getElementById('root'));
+    <Router>
+        <div>
+            <Header/>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/login" component={Login}/>
+        </div>
+    </Router>), document.getElementById('root'));
 registerServiceWorker();
