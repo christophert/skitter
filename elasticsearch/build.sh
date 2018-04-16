5b1dddc45b4bfe295558bd22ec9ab89e782739e3
Christopher Tran <me@christran.in>
2018-04-16 18:22:57 -0400

    resolve issues with ES and switch to ES container with dynamic cert provisioning

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-KEYSTORE_PASSWORD=$(cat private-pass)
 
-# generate certificates
-#/gocode/bin/cfssl gencert -remote=http://ca:8888 \
-#    -hostname=authentication csr.json
+# create ES cert directory
+mkdir -p $CERTS_DIR
 
 # get CA Certificate
 curl -XPOST -H "Content-Type: application/json" -d '{"label": "primary"}' \
     http://ca:8888/api/v1/cfssl/info > ca.json
 
-curl -X POST -H "Content-Type: application/json" -d @csr.json \
+curl -XPOST -H "Content-Type: application/json" -d @/csr.json \
     http://ca:8888/api/v1/cfssl/newcert > full_cert.json
 
 # my jank method of extracting json content b/c it takes way too long to install
@@ -22,36 +20,9 @@ extract_json_data () {
         sed 's/",.*//' | \
         sed 's/\\n/\n/g' > $3
 }
-echo $KEYSTORE_PASSWORD
 # make the private key and certificate request
-extract_json_data ca.json certificate ca.crt
-extract_json_data full_cert.json certificate certificate.crt
-extract_json_data full_cert.json private_key certificate.key
+extract_json_data ca.json certificate $CERTS_DIR/ca.crt
+extract_json_data full_cert.json certificate $CERTS_DIR/certificate.crt
+extract_json_data full_cert.json private_key $CERTS_DIR/certificate.key
 
-cat ca.crt
-cat certificate.crt
-
-cat ca.crt >> certificate.crt
-
-# export as pkcs12
-openssl pkcs12 -export -name elasticsearchssl -in certificate.crt \
-    -inkey certificate.key -out /usr/share/elasticsearch/elasticsearch-5.2.2/config/keystore.p12 \
-    -password pass:$KEYSTORE_PASSWORD
-
-
-# make a keystore for our application and store it in the correct folder
-keytool -importkeystore -destkeystore /usr/share/elasticsearch/elasticsearch-5.2.2/config/keystore.jks \
-    -srckeystore /usr/share/elasticsearch/elasticsearch-5.2.2/config/keystore.p12 -srcstoretype pkcs12 \
-    -alias elasticsearchssl -keypass $KEYSTORE_PASSWORD \
-    -storepass $KEYSTORE_PASSWORD -srckeypass $KEYSTORE_PASSWORD \
-    -destkeypass $KEYSTORE_PASSWORD < private-pass
-
-cp certificate.key /usr/share/elasticsearch/elasticsearch-5.2.2/config/certificate.key
-cp certificate.crt /usr/share/elasticsearch/elasticsearch-5.2.2/config/certificate.crt
-cp ca.crt /usr/share/elasticsearch/elasticsearch-5.2.2/config/ca.crt
-# clean unessessary files
-chmod -R 777 ./
-# run elasticsearch
-chown -R elastic:elastic ./
-sudo -H -u elastic /bin/bash -c /usr/share/elasticsearch/elasticsearch-5.2.2/bin/elasticsearch
-
+/usr/local/bin/docker-entrypoint.sh
