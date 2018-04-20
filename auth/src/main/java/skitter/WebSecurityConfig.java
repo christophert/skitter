f382e95feae100fc2a19447a3e2aa3434c555e0b
Christopher Tran <me@christran.in>
2018-04-19 23:40:51 -0400

    maybe this time

--- auth/src/main/java/skitter/WebSecurityConfig.java
+++ auth/src/main/java/skitter/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws SkitterUnauthorizedException {
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf()//.disable()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //                .requireCsrfProtectionMatcher(new RequestMatcher() {
