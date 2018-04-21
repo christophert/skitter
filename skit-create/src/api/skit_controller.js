6a66ff76ac685d91f0b8f1be4ff383f573eebde6
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-20 20:22:23 -0400

    Return 500 on elastic error

--- skit-create/src/api/skit_controller.js
+++ skit-create/src/api/skit_controller.js
@@ -11,23 +11,26 @@ module.exports.SkitController = function SkitController(service) {
     router.post('/AddSkit', function(req, res) {
         let msg = req.body.msg;
         return security.getUid(req)
+            .catch((err) => res.status(401).send({error:err}).end())
             .then((id) => service.addSkit(id, msg))
             .then(() => res.end())
-            .catch((err) => res.status(401).send({error:err}).end());
+            .catch((err) => res.status(500).send({error:err}).end());
     });
     router.delete('/RemoveSkit', function(req, res) {
         let skitId = req.query.skitId;
         return security.getUid(req)
+            .catch((err) => res.status(401).send({error:err}).end())
             .then((id) => service.deleteSkit(id, skitId))
             .then(() => res.end())
-            .catch((err) => res.status(401).send({error:err}).end());
+            .catch((err) => res.status(500).send({error:err}).end());
     });
     router.get('/GetSkits', function(req, res) {
         let id = req.query.id;
         return security.getUid(req)
+            .catch((err) => res.status(401).send({error:err}).end())
             .then(() => service.getSkits(id))
             .then((data) => res.send(data).end())
-            .catch((err) => res.status(401).send({error:err}).end());
+            .catch((err) => res.status(500).send({error:err}).end());
     });
     return router;
 };
