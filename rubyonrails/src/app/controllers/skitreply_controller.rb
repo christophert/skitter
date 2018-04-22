35276d6d7312260b70a51f352dbf71eca87121a8
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-22 00:43:09 -0400

    progressgit status!

--- rubyonrails/src/app/controllers/skitreply_controller.rb
+++ rubyonrails/src/app/controllers/skitreply_controller.rb
@@ -1,15 +1,51 @@
+require 'elasticsearch'
+
 class SkitreplyController < ApplicationController
   def AddSkitReply
-      reply = JSON.parse(request.raw_post)['reply']
-      uid = request.headers['X-SKITTER-AUTH-USER']
-      name = request.headers['X-SKITTER-AUTH-NAME']
-      if reply.to_s.empty? or reply.length > 140
-          render json: {}, status: :bad_request
+      @json = JSON.parse(request.raw_post)
+      @skitId = @json['skitId']
+      @reply = @json['reply']
+      @uid = request.headers['X-SKITTER-AUTH-USER']
+      @name = request.headers['X-SKITTER-AUTH-NAME']
+      if @reply.to_s.empty? or @reply.length > 140
+          render json: {error: 'reply must contain data, but be less than 140 characters'}, status: :bad_request
       end
-      if uid.to_s.empty? or name.to_s.empty?
-          render json: {}, status: :forbidden
+      if @skitId.to_s.empty? or @uid.to_s.empty? or @name.to_s.empty?
+          render json: {error: 'skitId empty, or skitter headers not set'}, status: :forbidden
       end
 
+      # ssl verification is borked in the ruby client????
+      @client = Elasticsearch::Client.new log: true, 
+          urls: 'https://elastic:s00pers3cur3pa$$word@skitdb:9200',
+          transport_options: { ssl: { verify: false } }
       
+      # elastic search exists does not work because it requires an ID (i love
+      # people design their own applications wrong)
+      #if !(@client.exists? index: @skitId, )
+      begin
+          @client.indices.create index: @skitId, body: { 
+              mappings: {
+                 replies: { 
+                     properties: {
+                      reply: { type: 'text' },
+                      name: { type: 'text'},
+                      uid: { type: 'text'},
+                      date: { type: 'date'}
+                     }
+                 } 
+              } 
+          }
+      rescue Elasticsearch::Transport::Transport::Errors::BadRequest
+        #already exists
+      end
+
+      @client.index index: @skitId, type: 'replies',
+          body: {
+            reply: @reply,
+            uid: @uid, 
+            name: @name, 
+            date: Time.now
+          }
+    
   end
 end
