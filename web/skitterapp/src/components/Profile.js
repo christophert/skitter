abc41ada632c03d95f4ae7331338f7ab8b96f164
Christopher Tran <me@christran.in>
2018-04-22 17:11:49 -0400

    add pull following and follow/unfollow

--- web/skitterapp/src/components/Profile.js
+++ web/skitterapp/src/components/Profile.js
@@ -18,10 +18,11 @@ export class ProfileCard extends Component {
         const userInfo = localStorage.getItem('userInfo');
         if(userInfo) {
             let uInfo = JSON.parse(userInfo);
-            this.state = { userInfo: uInfo, isFollowing: false, isSelf: props.isSelf, name: props.firstName + " " + props.lastname, username: props.username }
+            this.state = { userInfo: uInfo, isFollowing: false, isSelf: props.isSelf, name: props.firstName + " " + props.lastname, username: props.username, followingCount: 0}
         } else {
-            this.state = { userInfo: {}, isFollowing: false, isSelf: props.isSelf, name: props.firstName + " " + props.lastname, username: props.username }
+            this.state = { userInfo: {}, isFollowing: false, isSelf: props.isSelf, name: props.firstName + " " + props.lastname, username: props.username, followingCount: 0 }
         }
+        this.changeFollowStatus = this.changeFollowStatus.bind(this);
     }
 
     componentDidMount() {
@@ -30,16 +31,92 @@ export class ProfileCard extends Component {
         }
     }
 
+    getFollowingData() {
+        this.setState({isLoading: true});
+        const { cookies } = this.props;
+        let urlParams = new URLSearchParams();
+        urlParams.append("uid", this.state.username);
+        fetch(`/follows/GetFollowers?${urlParams.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'X-XSRF-TOKEN': cookies.get('XSRF-TOKEN')
+            }
+        })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error("Response is not OK");
+                }
+                this.setState({isLoading: false});
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({following: data, followingCount: data.length, isLoading: false });
+            })
+            .catch((error) => this.setState({error, isLoading: false}));
+    }
 
+    changeFollowStatus(uid, follow) {
+        this.setState({isLoading: true});
+        const { cookies } = this.props;
+        if(follow===true) {
+            fetch('/follows/FollowUser', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'X-XSRF-TOKEN': cookies.get('XSRF-TOKEN')
+                },
+                body: JSON.stringify({
+                    follow: uid
+                })
+            })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error("Could not follow");
+                }
+                this.setState({isLoading: false});
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({ isFollowing: true });
+            })
+            .catch((error) => this.setState({error, isLoading: false}));
+        } else {
+            //follow false
+            let urlParams = new URLSearchParams();
+            urlParams.append("uid", uid);
+            fetch(`/follows/UnfollowUser?${urlParams.toString()}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'X-XSRF-TOKEN': cookies.get('XSRF-TOKEN')
+                }
+            })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error("Could not unfollow");
+                }
+                this.setState({isLoading: false});
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({ isFollowing: false });
+            })
+            .catch((error) => this.setState({error, isLoading: false}));
+        }
+    }
 
     render() {
+        if(this.state.error) {
+            return (<p>There was an error retrieving this profile</p>);
+        }
         let followButton = [];
         if (!this.state.isSelf) { //no follow button
             followButton.push(<br key="br" />);
             if(this.state.isFollowing) {
-                followButton.push(<Button block color="secondary" key="bun">Unfollow</Button>);
+                followButton.push(<Button block color="secondary" key="bun" onClick={() => this.changeFollowStatus(this.state.username, false)}>Unfollow</Button>);
             } else {
-                followButton.push(<Button block className="bg-purple" key="bf">Follow</Button>);
+                followButton.push(<Button block className="bg-purple" key="bf" onClick={() => this.changeFollowStatus(this.state.username, true)}>Follow</Button>);
             }
         }
         return (
@@ -56,9 +133,7 @@ export class ProfileCard extends Component {
                         <CardSubtitle>@{this.state.username}</CardSubtitle>
                         <hr/>
                         <Row>
-                            <Col xs="4" className="border border-top-0 border-bottom-0 border-left-0"><small>Skits</small><br/>123</Col>
-                            <Col xs="4" className="border border-top-0 border-bottom-0 border-left-0"><small>Followers</small><br/>123</Col>
-                            <Col xs="4"><small>Following</small><br/>123</Col>
+                            <Col xs="12"><small>Following</small><br/>{this.state.followingCount}</Col>
                         </Row>
                         {followButton}
                     </CardBody>
