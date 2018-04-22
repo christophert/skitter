ad62d6b3cc28797ceb30bc823c20727e4202a647
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-22 02:11:09 -0400

    Add is up and runninggit add -A!

--- rubyonrails/src/app/controllers/skitreply_controller.rb
+++ rubyonrails/src/app/controllers/skitreply_controller.rb
@@ -5,6 +5,7 @@ class SkitreplyController < ApplicationController
       @json = JSON.parse(request.raw_post)
       @skitId = @json['skitId']
       @reply = @json['reply']
+      @otherUid = @json['uid']
       @uid = request.headers['X-SKITTER-AUTH-USER']
       @name = request.headers['X-SKITTER-AUTH-NAME']
       if @reply.to_s.empty? or @reply.length > 140
@@ -19,33 +20,27 @@ class SkitreplyController < ApplicationController
           urls: 'https://elastic:s00pers3cur3pa$$word@skitdb:9200',
           transport_options: { ssl: { verify: false } }
       
-      # elastic search exists does not work because it requires an ID (i love
-      # people design their own applications wrong)
-      #if !(@client.exists? index: @skitId, )
-      begin
-          @client.indices.create index: @skitId, body: { 
-              mappings: {
-                 replies: { 
-                     properties: {
-                      reply: { type: 'text' },
-                      name: { type: 'text'},
-                      uid: { type: 'text'},
-                      date: { type: 'date'}
-                     }
-                 } 
-              } 
-          }
-      rescue Elasticsearch::Transport::Transport::Errors::BadRequest
-        #already exists
-      end
+      @oldskit = (@client.search index: @otherUid, id: @skitId)['hits']['hits'][0]
 
-      @client.index index: @skitId, type: 'replies',
-          body: {
-            reply: @reply,
-            uid: @uid, 
-            name: @name, 
-            date: Time.now
-          }
-    
+      @toadd = {
+                  msg: @reply,
+                  name: @name, 
+                  uid: @uid,
+                  date: Time.now
+      }
+      puts @toadd
+
+      @client.update index: @otherUid, type: 'skits', id: @skitId, 
+          body: { script: { source: 'ctx._source.replies.add(params.reply)', params: { reply: @toadd } } }
+
+  end
+
+  def GetSkitReply
+      @client = Elasticsearch::Client.new log: true, 
+          urls: 'https://elastic:s00pers3cur3pa$$word@skitdb:9200',
+          transport_options: { ssl: { verify: false } }
+      @id = request.query_parameters['id']
+      @result = @client.search index: 'jfb3657'
+      render json: @result
   end
 end
--- rubyonrails/src/config/routes.rb
+++ rubyonrails/src/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'skitreply/GetSkitReply'
+
   post 'skitreply/AddSkitReply'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
