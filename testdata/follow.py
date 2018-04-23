cfd42f1b5dfb305717a468a00e1d1ebe5b79180f
joebartelmo <joebartelmo@gmail.com>
2018-04-22 22:40:53 -0400

    ignore sls

--- testdata/follow.py
+++ testdata/follow.py
@@ -7,21 +7,21 @@ xheaders={"X-SKITTER-AUTH-USER": "jfb3657", \
 # assumes jfb3657 and ctt1414 are in the db
 
 print('Testing add follower no data..')
-r = requests.post("https://localhost/follows/FollowUser", headers=xheaders, data={})
+r = requests.post("https://localhost/follows/FollowUser", headers=xheaders, data={}, verify=False)
 assert r.status_code >= 400
 
 print('Testing add follower no headers..')
-r = requests.post("https://localhost/follows/FollowUser", data={"follow": "ctt1414"})
+r = requests.post("https://localhost/follows/FollowUser", data={"follow": "ctt1414"}, verify=False)
 assert r.status_code >= 400
 
 print('Testing add follower success..')
-r = requests.post("https://localhost/follows/FollowUser", headers=xheaders, data={"follow": "ctt1414"})
+r = requests.post("https://localhost/follows/FollowUser", headers=xheaders, data={"follow": "ctt1414"}, verify=False)
 assert r.status_code >= 200 and r.status_code <= 315
 
 print('Get Followers..')
-r = requests.post("https://localhost/follows/GetFollowers?uid=jfb3657", headers=xheaders)
+r = requests.post("https://localhost/follows/GetFollowers?uid=jfb3657", headers=xheaders, verify=False)
 assert r.status_code >= 200 and r.status_code <= 315
 
 print('Get Userssearch..')
-r = requests.post("https://localhost/follows/UserSearch?uid=jfb", headers=xheaders)
+r = requests.post("https://localhost/follows/UserSearch?uid=jfb", headers=xheaders, verify=False)
 assert r.status_code >= 200 and r.status_code <= 315
--- testdata/skits.py
+++ testdata/skits.py
@@ -5,17 +5,17 @@ xheaders={"X-SKITTER-AUTH-USER": "testuser", \
          "X-XSRF-TOKEN": "dummytoken"}
 addskit = {}
 print('Testing add skit no data..')
-r = requests.post("https://localhost/skits/AddSkit", headers=xheaders, data={})
+r = requests.post("https://localhost/skits/AddSkit", headers=xheaders, data={}, verify=False)
 assert r.status_code >= 400
 
 print('Testing add skit no headers..')
-r = requests.post("https://localhost/skits/AddSkit", data={})
+r = requests.post("https://localhost/skits/AddSkit", data={}, verify=False)
 assert r.status_code >= 400
 
 print('Testing valid skit')
-skit = requests.post("https://localhost/skits/AddSkit", headers=xheaders, data={"msg":"this is a test"})
+skit = requests.post("https://localhost/skits/AddSkit", headers=xheaders, data={"msg":"this is a test"}, verify=False)
 assert skit.status_code >= 200 and skit.status_code <= 315
 
 print('Testing skit retrieve')
-r = requests.get("https://localhost/skits/GetSkits?id=testuser", headers=xheaders)
+r = requests.get("https://localhost/skits/GetSkits?id=testuser", headers=xheaders, verify=False)
 assert r.status_code >= 200 and r.status_code <= 315
